ce8d3250227f5bc041151ecf609e248528e602e7
OlgaY <yurkova.o@gmail.com>
2022-08-03 22:14:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//let calculate = document.getElementById("calculateId");
+//console.let(calculate);
+
+//let firstSum = prompt("7");
+
+//let secondSum = prompt("7");
+
+let first;
+first = prompt("enter first number");
+console.log(first);
+
+let second;
+second = prompt("enter second number");
+console.log(second);
+
+let firstNumber;
+firstNumber = parseFloat(first);
+console.log(firstNumber);
+
+let secondNumber;
+secondNumber = parseFloat(second);
+console.log(secondNumber);
+
+let sum;
+sum = firstNumber + secondNumber;
+console.log(sum);
+
+alert("sum: " + sum);
+
+
